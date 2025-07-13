65cdec8fecaa524946d8944bc6e31e06b51d58da
codewithshoaib.dev <codewithshoaib.dev@gmail.com>
2025-07-13 13:40:45 +0500

    fixed misconfiguration issue

--- {{ project_name }}/urls.py
+++ {{ project_name }}/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapi.users.urls')),
+    path('api/', include('users.urls')),
 
 
 ]
